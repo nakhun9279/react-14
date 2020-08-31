105a30fe59fa6b49b9cdb69b9a4e3cd978977783
nakhun9279 <fjfj9279@naver.com>
2020-08-31 10:59:40 +0900

    14.4.2 NewList 만들기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,8 @@
-import React, { useState } from 'react';
-import axios from 'axios';
+import React from 'react';
+import NewsList from './components/NewsList';
  
 const App = () => {
-  const [data, setData] = useState(null);
-  const onClick = async () => {
-    try {
-      const response = await axios.get(
-        'http://newsapi.org/v2/top-headlines?country=kr&apiKey=03285884c7d44ffb90c5522eb1fe467b'
-      );
-      setData(response.data);
-    } catch (e) {
-      console.log(e);
-    }
-  };
-  return (
-    <div>
-      <div>
-        <button onClick={onClick}>불러오기</button>
-      </div>
-      {data && <textarea rows={7} value={JSON.stringify(data, null, 2)} readOnly={true} />}
-    </div>
-  );
+  return <NewsList />;
 };
  
 export default App;
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,62 +1,45 @@
 import React from 'react';
 import styled from 'styled-components';
+import NewsItem from './NewsItem';
 
 
 const NewsItemBlock = styled.div`
-  display: flex;
-
-
-
-.thumbnail {
-    margin-right: 1rem;
-    img {
-      display: block;
-      width: 160px;
-      height: 100px;
-      object-fit: cover;
-    }
-  }
-  .contents {
-    h2 {
-      margin: 0;
-      a {
-        color: black;
-      }
-    }
-    p {
-      margin: 0;
-      line-height: 1.5;
-      margin-top: 0.5rem;
-      white-space: normal;
-    }
-  }
-  & + & {
-    margin-top: 3rem;
-  }
+  
+box-sizing: border-box;
+padding-bottom: 3rem;
+width: 768px;
+margin: 0 auto;
+margin-top:2rem;
+@media screen and (max-width: 768px) {
+    width : 100%;
+    padding-left: 1rem;
+    padding-right: 1rem;
+}
 `;
-const NewsItem = ({ article }) => {
-  const { title, description, url, urlToImage } = article;
-  return (
-    <NewsItemBlock>
-      {urlToImage && (
-        <div className="thumbnail">
-          <a href={url} target="_blank" rel="noopener noreferrer">
-            <img src={urlToImage} alt="thumbnail" />
-          </a>
-        </div>
-      )}
-      <div className="contents">
-        <h2>
-          <a href={url} target=" blank" rel="noopener noreferrer">
-            {title}
-          </a>
-        </h2>
-        <p>{description}</p>
-      </div>
-    </NewsItemBlock>
-  );
-};
 
 
-
-export default NewsItem;
+const sampleArticle = {
+    title: '제목',
+    description: '내용',
+    url: 'https://google.com',
+    urlToImage: 'https://via.placeholder.com/160',
+  };
+  
+  
+  
+  const NewsList = () => {
+    return (
+      <NewsListBlock>
+        <NewsItem article={sampleArticle} />
+        <NewsItem article={sampleArticle} />
+        <NewsItem article={sampleArticle} />
+        <NewsItem article={sampleArticle} />
+        <NewsItem article={sampleArticle} />
+        <NewsItem article={sampleArticle} />
+      </NewsListBlock>
+    );
+  };
+  
+  
+  
+  export default NewsList;
