fe577cf3eec3d0de51c254b2279f1e7156ef53c4
nakhun9279 <fjfj9279@naver.com>
2020-08-31 11:03:29 +0900

    14.6.1 카테고리 선택 UI만들기

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -18,24 +18,42 @@ margin-top:2rem;
 `;
 
 
-const sampleArticle = {
-    title: '제목',
-    description: '내용',
-    url: 'https://google.com',
-    urlToImage: 'https://via.placeholder.com/160',
-  };
-  
-  
-  
-  const NewsList = () => {
+const NewsList = () => {
+    const [articles, setArticles] = useState(null);
+    const [loading, setLoading] = useState(false);
+   
+    useEffect(() => {
+      // async를 사용하는 함수 따로 선언
+      const fetchData = async () => {
+        setLoading(true);
+        try {
+          const response = await axios.get(
+            'http://newsapi.org/v2/top-headlines?country=kr&apiKey=03285884c7d44ffb90c5522eb1fe467b',
+          );
+          setArticles(response.data.articles);
+        } catch (e) {
+          console.log(e);
+        }
+        setLoading(false);
+      };
+      fetchData();
+    }, []);
+   
+    // 대기 중일 때
+    if (loading) {
+      return <NewsListBlock>대기 중…</NewsListBlock>;
+    }
+    // 아직 articles 값이 설정되지 않았을 때
+    if (!articles) {
+      return null;
+    }
+   
+    // articles 값이 유효할 때
     return (
       <NewsListBlock>
-        <NewsItem article={sampleArticle} />
-        <NewsItem article={sampleArticle} />
-        <NewsItem article={sampleArticle} />
-        <NewsItem article={sampleArticle} />
-        <NewsItem article={sampleArticle} />
-        <NewsItem article={sampleArticle} />
+        {articles.map(article => (
+          <NewsItem key={article.url} article={article} />
+        ))}
       </NewsListBlock>
     );
   };
